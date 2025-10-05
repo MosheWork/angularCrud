34abf766157f40822b37273f5d33d63cba9ba31f
moshe maman <moshework1985@gmail.com>
2025-10-05 14:37:34 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TelemetryService } from './services/core/telemetry.service';
 
-
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,34 @@ import { TelemetryService } from './services/core/telemetry.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private telemetry: TelemetryService) {}
-  ngOnInit() {
-    this.telemetry.init();   // להתחיל להאזין לניווטים
+  private readonly APP_NAME = 'ONN-LINE'; // suffix, change if you want
+
+  constructor(
+    private telemetry: TelemetryService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private title: Title
+  ) {}
+
+  ngOnInit(): void {
+    // keep your telemetry init
+    this.telemetry.init();
+
+    // set <title> based on the current route's data.title
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map(() => {
+          // walk to the deepest activated child
+          let r = this.route;
+          while (r.firstChild) r = r.firstChild;
+          return r;
+        }),
+        mergeMap((r) => r.data) // { title: '...' } from your routes
+      )
+      .subscribe((data) => {
+        const routeTitle = data?.['title'] as string | undefined;
+        this.title.setTitle(routeTitle ? `${routeTitle} | ${this.APP_NAME}` : this.APP_NAME);
+      });
   }
 }
